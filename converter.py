d6e82bb0ca7d785874156cd693db36bfb0559a80
paulfaa <paulfarrell12@gmail.com>
2020-12-08 21:31:04 +0000

    Started implementing GUI

--- converter.py
+++ converter.py
@@ -1,13 +1,36 @@
 import os
 import re
+import logging
+import tkinter as tk
+from tkinter import filedialog, Text
+
+#Script to read the old 'sounds.txt' file from Garry's Mod round end plugin
+#and create txt files for each mathcing the new format
+#TODO: add GUI + function to generate txt files using id3 tags
 
 currentPath = os.getcwd()
 folderPath = ""
 
-#Script to read the old 'sounds.txt' file from Garry's Mod round end plugin
-#and create txt files for each mathcing the new format
+def selectFile():
+	fileName = filedialog.askopenfilename(initialdir="/", title = "Select sounds.txt",
+	filetypes = (("Text File", "*.txt"),) )
+	print(fileName)
+
+def createGUI():
+	root = tk.Tk()
+	canvas = tk.Canvas(root, height=200, width=300)
+	canvas.pack()
+
+	openFile = tk.Button(root, text="Select file", padx=5, pady=5, command=selectFile)
+	openFile.pack()
+	convertFile = tk.Button(root, text="Convert file", padx=5, pady=5)
+	convertFile.pack
+
+	root.mainloop()
+
 
 def convert():
+	countFiles = 0
 	try:
 		with open (currentPath + "\sounds.txt", "r") as songFile:
 			next(songFile) #skip header
@@ -15,27 +38,31 @@ def convert():
 				matches = re.findall(r"""([^"]*)""", line)
 				fileName = matches[2]
 				fileName = re.sub('.mp3$', '.txt', fileName)
-				print(fileName)
+				#print(fileName)
 				artist = matches [6]
 				track = matches [10]
-				print(fileName + ", " + artist + ", " + track)
-				#update this for TIMEOUT
-				if line[13] == "T":
+				#print(fileName + ", " + artist + ", " + track)
+				if line[14] == "R":
 					folderPath = "\\"+"traitor"+"\\"
-				if line[13] == "I":
+				if line[14] == "N":
 					folderPath = "\\"+"innocent"+"\\"
-				print("Folderpath: " + currentPath + folderPath + fileName)
+				if line[14] == "I":
+					folderPath = "\\"+"timeout"+"\\"
+				#print("Folderpath: " + currentPath + folderPath + fileName)
 				fullPath = os.path.join(currentPath + folderPath)
-				print(fullPath)
+				#print(fullPath)
 				try:
 					if not os.path.exists(fullPath):
 						os.makedirs(fullPath)
 					if not os.path.exists(fullPath + fileName):
 						with open(fullPath + fileName, 'w') as outputFile:
 							outputFile.write(track + '\n' + artist)
+							countFiles = countFiles + 1
 				except:
-					print("Write file failed, check sounds.txt has no illegal characters")
+					logging.exception("Write file failed, check sounds.txt has no illegal characters")
+		print("Script complete, created " + str(countFiles) + " files.")
 	except:
-		print("Read file failed, check sounds.txt is in same directory as script")
+		logging.exception("Read file failed, check sounds.txt is in same directory as script")
 		
-convert()
+selectFile()
+#convert()
